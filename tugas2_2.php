ff8e62fcf78f0a187c0df231f29697855d814bb0
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-07 12:09:06 +0700

    Add files via upload

---
+++ tugas2_2.php
@@ -0,0 +1,9 @@
+<?php
+
+$harga_beli = 50000;
+$harga_jual = 80000;
+$persentase = $harga_beli/$harga_jual*100;
+
+echo "Jual = ".$harga_jual,"<br>","Beli = ".$harga_beli,"<br>","Keuntungan = ?","<br>","Jawaban : ".$persentase,"%";
+
+?>
